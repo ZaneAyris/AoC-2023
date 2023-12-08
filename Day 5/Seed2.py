f94ed1f1c1559cb46778c8af74aa885a1c0b9c25
ZaneAyris <108333278+ZaneAyris@users.noreply.github.com>
2023-12-08 17:47:07 +1300

    Update Seed2.py

--- Day 5/Seed2.py
+++ Day 5/Seed2.py
@@ -1,48 +1,58 @@
-def parseFromRange(start, rangeValue):
-    return start + rangeValue -1
-
-def parseToRange(start, end):
-    return end-start+1
-
-    
 def mapX(inputList, nextList):
-    outputList = []
+    outputSet = set()
 
-    allInputLists = inputList
+    allInputLists = inputList.copy()
 
     while len(allInputLists) != 0:
+        
         selection = allInputLists[0]
 
         inpStart, inpRange = [n for n in selection]
         inpEnd = inpStart + inpRange -1
+        completed = False
+        # (Start, Range)
         
         for numbers in nextList:
             destStart, sourStart, rangeLen = [int(s) for s in numbers.split()]
             sourEnd = sourStart + rangeLen - 1
-
-            #6 cases
-            #If a part of the range isnt connected with one of the functions, keep it for the future checks ie a unconnected range is checked against all ranges of source
-            #If input range is a subset of output range
+            destEnd = destStart + rangeLen - 1
             if inpStart >= sourStart and  inpEnd <= sourEnd:
-                startRange = inpStart - sourStart
+                startRange = inpStart - sourStart +1
                 endRange = inpEnd - sourStart +1
-                outputForm = (destStart + startRange, destStart + endRange)
-                outputList.append(outputForm)
+                outputForm = (destStart + startRange -1, endRange)
+                outputSet.add(outputForm)
+                completed = True
                 break
 
             elif inpStart < sourStart <= inpEnd <= sourEnd:
                 # First add matching
                 connectedEndRange = inpEnd - sourStart + 1
-                connectedOutputForm = (sourStart, destStart + connectedEndRange)
-                outputList.append(connectedOutputForm)
+                connectedOutputForm = (destStart, connectedEndRange)
+                outputSet.add(connectedOutputForm)
                 # leave what has not been connected
-                disconnectedEnd = sourStart - inpStart
                 inpEnd = sourStart -1
 
-            elif sourStart <= inpStart <= inpEnd < sourEnd:
+            elif sourStart <= inpStart <=  sourEnd < inpEnd:
+                connectedRange = sourEnd - inpStart + 1
+                connectedOutputForm = (destStart, connectedRange)
+                outputSet.add(connectedOutputForm)
+                inpStart = sourEnd + 1
 
+            elif inpStart < sourStart < sourEnd < inpEnd:
+                allInputLists.append((sourEnd, inpEnd + 1))
+                # this is whats left allInputLists.append((inpStart, sourStart +1))
+                connectedOutputForm = (destStart, rangeLen)
+                outputSet.add(connectedOutputForm)
+                inpEnd = sourStart +1
+                
+            else:
+                pass
+
+        if completed == False:
+            outputSet.add((inpStart, inpEnd - inpStart + 1))
+                    
         allInputLists = allInputLists[1:]
-    return outputList
+    return list(outputSet)
 
 
 file = open("input.txt","r")
@@ -58,8 +68,14 @@ seedRanges = []
 for i in range(0,len(seeds),2):
     seedRanges.append((seeds[i], seeds[i+1]))
 
-poo = mapX(seedRanges, soil)
-print(mapX(poo, fert))
+seedToSoil = mapX(seedRanges, soil)
+soilToFert = mapX(seedToSoil, fert)
+fertToWater = mapX(soilToFert, water)
+waterToLight = mapX(fertToWater, light)
+lightToTemp = mapX(waterToLight, temp)
+tempToHumid = mapX(lightToTemp, humid)
+humidToLocation = mapX(tempToHumid, location)
+sorted_list = sorted(humidToLocation, key=lambda x: x[0])
 
-
+print(sorted_list)
 
