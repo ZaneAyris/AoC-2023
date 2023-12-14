3f19acb248763a7799c9748e050d045c00ff1409
ZaneAyris <108333278+ZaneAyris@users.noreply.github.com>
2023-12-14 16:06:47 +1300

    Duplication D5 P2 for another attempt

---
+++ Day 5/Seed2Round2.py
@@ -0,0 +1,32 @@
+file = open("input.txt","r")
+sections = file.read().split("\n\n")
+
+def mapX(currObject, nextList):
+    currObject = int(currObject)
+    for numbers in nextList:
+        destStart, sourStart, rangeLen = [int(s) for s in numbers.split()]
+        if currObject >= sourStart and currObject < sourStart + rangeLen - 1:
+            return currObject - sourStart + destStart
+    return sourStart
+
+seeds = sections[0].split(":")[1].strip().split(" ")
+
+soil, fert, water, light, temp, humid, location = [section.split(":")[1].strip().split("\n") for section in sections[1:]]
+
+minLocation = None
+
+for seed in seeds:
+    dirt = mapX(seed, soil)
+    fertalizer = mapX(dirt, fert)
+    h2o = mapX(fertalizer, water)
+    sun = mapX(h2o, light)
+    heat = mapX(sun, temp)
+    hummas = mapX(heat, humid)
+    local = mapX(hummas, location)
+    if minLocation == None:
+        minLocation = local
+    if local < minLocation:
+        minlocation = local
+        
+print(local)
+
